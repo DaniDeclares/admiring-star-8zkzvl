4b69973842394d03ce2a000ad1a3490c6b9654bb
Dani Declares <danideclarens@gmail.com>
2025-05-17 19:40:15 -0400

    Prune App.js routes to only existing pages and remove missing LegalPage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
+// src/App.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
+// Global styles & resets
+import "./index.css";
 
-import Homepage from "./pages/Homepage.jsx";
-import ShopPage from "./pages/ShopPage.jsx";
-import CoachingPage from "./pages/CoachingPage.jsx";
-import EventsPage from "./pages/EventsPage.jsx";
-import WeddingsPage from "./pages/WeddingsPage.jsx";
-import LegalPage from "./pages/LegalPage.jsx";
-import BlogPage from "./pages/BlogPage.jsx";
-import ContactPage from "./pages/ContactPage.jsx";
-import NotaryPage from "./pages/NotaryPage.jsx";
+// Site chrome
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// Pages
+import Homepage from "./pages/Homepage";
+import BlogPage from "./pages/BlogPage";
+import CoachingPage from "./pages/CoachingPage";
+import ContactPage from "./pages/ContactPage";
+import EventsPage from "./pages/EventsPage";
+import NotaryPage from "./pages/NotaryPage";
+import ShopPage from "./pages/ShopPage";
+import WeddingsPage from "./pages/WeddingsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/shop" element={<ShopPage />} />
-          <Route path="/coaching" element={<CoachingPage />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/weddings" element={<WeddingsPage />} />
-          <Route path="/legal" element={<LegalPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/notary" element={<NotaryPage />} />
-        </Routes>
-      </main>
+
+      <Routes>
+        {/* homepage on “/” */}
+        <Route path="/" element={<Homepage />} />
+
+        {/* core site pages */}
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/coaching" element={<CoachingPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/notary" element={<NotaryPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/weddings" element={<WeddingsPage />} />
+
+        {/* fallback to homepage (or swap in a 404 component if you add one) */}
+        <Route path="*" element={<Homepage />} />
+      </Routes>
+
       <Footer />
     </BrowserRouter>
   );
