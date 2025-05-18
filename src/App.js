e35e3980891adebb45f3961907111498259ee69e
Dani Declares <danideclarens@gmail.com>
2025-05-17 21:48:08 -0400

    Update App.js: add global index.css import, catch-all route

--- src/App.js
+++ src/App.js
@@ -1,49 +1,66 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-// Global styles & resets
+// global styles (reset, fonts, etc.)
 import "./index.css";
 
-// Site chrome
+// site-wide chrome
+import FestivalBanner from "./components/FestivalBanner";
+import "./components/FestivalBanner.css";
 import Navbar from "./components/Navbar";
+import "./components/Navbar.css";
+import SocialLinks from "./components/SocialLinks";
+import "./components/SocialLinks.css";
+import CookieConsent from "./components/CookieConsent";
+import "./components/CookieConsent.css";
 import Footer from "./components/Footer";
+import "./components/Footer.css";
 
-// Pages
+// pages + their styles
 import Homepage from "./pages/Homepage";
+import "./pages/Homepage.css";
 import BlogPage from "./pages/BlogPage";
-import CoachingPage from "./pages/CoachingPage";
-import ContactPage from "./pages/ContactPage";
-import EventsPage from "./pages/EventsPage";
-import NotaryPage from "./pages/NotaryPage";
+import "./pages/BlogPage.css";
 import ShopPage from "./pages/ShopPage";
+import "./pages/ShopPage.css";
+import CoachingPage from "./pages/CoachingPage";
+import "./pages/CoachingPage.css";
+import EventsPage from "./pages/EventsPage";
+import "./pages/EventsPage.css";
 import WeddingsPage from "./pages/WeddingsPage";
+import "./pages/WeddingsPage.css";
+import NotaryPage from "./pages/NotaryPage";
+import "./pages/NotaryPage.css";
+import ContactPage from "./pages/ContactPage";
+import "./pages/ContactPage.css";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
+      {/* top banner & nav */}
+      <FestivalBanner />
       <Navbar />
 
+      {/* dynamic page content */}
       <Routes>
-        {/* homepage on “/” */}
         <Route path="/" element={<Homepage />} />
-
-        {/* core site pages */}
         <Route path="/blog" element={<BlogPage />} />
-        <Route path="/coaching" element={<CoachingPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/notary" element={<NotaryPage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/coaching" element={<CoachingPage />} />
+        <Route path="/events" element={<EventsPage />} />
         <Route path="/weddings" element={<WeddingsPage />} />
+        <Route path="/notary" element={<NotaryPage />} />
+        <Route path="/contact" element={<ContactPage />} />
 
-        {/* fallback to homepage (or swap in a 404 component if you add one) */}
-        <Route path="*" element={<Homepage />} />
+        {/* catch-all: redirect invalid URLs home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
+      {/* footer chrome */}
+      <SocialLinks />
+      <CookieConsent />
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
-
-export default App;
