c63947a0bbc8d1af3e5af2fefcf077fe39bd779c
Dani Declares <danideclarens@gmail.com>
2025-05-21 20:02:49 -0400

    feat: add Helmet SEO tags to Homepage and wrap app with HelmetProvider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
-import { HelmetProvider } from "react-helmet-async"; // Add this
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
