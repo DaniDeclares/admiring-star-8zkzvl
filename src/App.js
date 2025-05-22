09a58d30bae4e28661deeee8d17b267b95267ffe
Dani Declares <danideclarens@gmail.com>
2025-05-22 17:47:40 -0400

    fix: remove duplicate App declaration and imports

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FestivalBanner from "./components/FestivalBanner.jsx";
-import Navbar from "./components/Navbar.jsx";
-import SocialLinks from "./components/SocialLinks.jsx";
-import CookieConsent from "./components/CookieConsent.jsx";
-import Footer from "./components/Footer.jsx";
-
-export default function App() {
-  return (
-    <Router>
-      <FestivalBanner />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<div>Hello from App</div>} />
-      </Routes>
-      <SocialLinks />
-      <CookieConsent />
-      <Footer />
-    </Router>
-  );
-}
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FestivalBanner from "./components/FestivalBanner.jsx";
-// (leave others commented out)
 
 export default function App() {
   return (
