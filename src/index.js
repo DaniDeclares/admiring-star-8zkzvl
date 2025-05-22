94bcb2d21f01fd69b34f280e98bc589faf10aff1
Dani Declares <danideclarens@gmail.com>
2025-05-22 10:24:29 -0400

    test: isolate React render to debug white screen

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
-import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext.jsx";
-import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <CartProvider>
-      <HelmetProvider> {/* Wrap App */}
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </HelmetProvider>
-    </CartProvider>
-  </React.StrictMode>
+  <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+    If you can see this, React is working.
+  </div>
 );
