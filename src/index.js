6ef114b738a64e7e4920935bc293de450cafd694
Dani Declares <danideclarens@gmail.com>
2025-05-21 20:28:07 -0400

    fix: add .jsx extension to CartContext import for ESM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./context/CartContext.jsx";
 import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
