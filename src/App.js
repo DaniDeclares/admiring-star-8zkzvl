ed612074060467998479c404a1d5016efef04a4f
Dani Declares <danideclarens@gmail.com>
2025-05-22 10:35:44 -0400

    test: reintroduce App with router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FestivalBanner from "./components/FestivalBanner.jsx";
-import Navbar from "./components/Navbar.jsx";
-import SocialLinks from "./components/SocialLinks.jsx";
-import CookieConsent from "./components/CookieConsent.jsx";
-import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
     <Router>
-      <FestivalBanner />
-      <Navbar />
       <Routes>
-        <Route path="/" element={<div>Hello World</div>} />
+        <Route path="/" element={<div>Hello from App</div>} />
       </Routes>
-      <SocialLinks />
-      <CookieConsent />
-      <Footer />
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-    If you can see this, React is working.
-  </div>
-);
+root.render(<App />);
