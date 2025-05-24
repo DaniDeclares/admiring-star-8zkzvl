3bb578d92555b187ecb98db0bb64537e9c60cc2e
Dani Declares <danideclarens@gmail.com>
2025-05-24 11:28:06 -0400

    fix: import FestivalBanner and render homepage content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import FestivalBanner from "./components/FestivalBanner.jsx";
 import Navbar from "./components/Navbar.jsx";
 import SocialLinks from "./components/SocialLinks.jsx";
 import CookieConsent from "./components/CookieConsent.jsx";
 import Footer from "./components/Footer.jsx";
 
 import Homepage from "./pages/Homepage.jsx";
-import CoachingPage from "./pages/CoachingPage.jsx"; // or any other safe, working page
+import CoachingPage from "./pages/CoachingPage.jsx";
 
 export default function App() {
   return (
