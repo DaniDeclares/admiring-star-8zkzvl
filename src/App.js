61de8a761bac9cc77f802c7c009e0dba8db636be
Dani Declares <danideclarens@gmail.com>
2025-05-24 10:58:55 -0400

    feat: add layout components to homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-// src/App.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
+// Layout
+import FestivalBanner from "./components/FestivalBanner.jsx";
+import Navbar from "./components/Navbar.jsx";
+import SocialLinks from "./components/SocialLinks.jsx";
+import CookieConsent from "./components/CookieConsent.jsx";
+import Footer from "./components/Footer.jsx";
+
+// Pages
 import Homepage from "./pages/Homepage.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-    </Routes>
+    <>
+      <FestivalBanner />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      <SocialLinks />
+      <CookieConsent />
+      <Footer />
+    </>
   );
 }
