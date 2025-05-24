285acf51e5eafcab63127c82d1678a72de1adec3
Dani Declares <danideclarens@gmail.com>
2025-05-24 10:53:47 -0400

    chore: add context and routing providers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { CartProvider } from "./context/CartContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <CartProvider>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </CartProvider>
+  </React.StrictMode>
+);
