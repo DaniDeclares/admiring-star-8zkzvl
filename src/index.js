ec9b09b0b0c76573feff6c9f56229791fbeaa531
Dani Declares <danideclarens@gmail.com>
2025-05-24 10:51:19 -0400

    chore: restore minimal baseline to isolate blank screen

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
-import { CartProvider } from "./context/CartContext.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <CartProvider>
-      <HelmetProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </HelmetProvider>
-    </CartProvider>
-  </React.StrictMode>
-);
+root.render(<App />);
