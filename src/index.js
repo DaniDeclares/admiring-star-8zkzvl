745e77b8238d80158cec75fc2668a16eae7645ca
Dani Declares <danideclarens@gmail.com>
2025-05-24 00:05:03 -0400

    fix: wrap App in CartProvider and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
+import { CartProvider } from "./context/CartContext.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <CartProvider>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </CartProvider>
+  </React.StrictMode>
+);
