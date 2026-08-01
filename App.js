891da6c776d4d678536da847b1fb6ec8cebab7cd
DaniDeclares <danideclaresns@gmail.com>
2026-08-01 11:12:53 -0400

    Delete App.js

--- App.js
+++
@@ -1,4 +0,0 @@
-// Root App archived: re-export canonical src/App
-// This file remains to avoid breaking tools that import from the repo root.
-// The canonical App is src/App.js
-export { default } from "./src/App";
