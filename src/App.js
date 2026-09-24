7dd49759d26b40d70807da9cdb7424d61d75725d
DaniDeclares <danideclaresns@gmail.com>
2026-09-23 21:56:39 -0400

    Add resilient one-time provider account recovery (#377)
    * Add one-time provider support recovery endpoint
    
    * Add secure provider support recovery page
    
    * Track server-only portal recovery token schema
    
    * Route one-time provider support recovery
    
    * Send scanner-safe provider recovery links through Resend
    
    * Allow providers to request scanner-safe recovery email
    
    * Avoid apostrophe lint failure in recovery copy
    
    * Import provider support recovery route component

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ import PortalAccessPage from "./pages/PortalAccessPage.jsx";
 import PortalLoginPage from "./pages/PortalLoginPage.jsx";
 import PortalForgotPasswordPage from "./pages/PortalForgotPasswordPage.jsx";
 import PortalResetPasswordPage from "./pages/PortalResetPasswordPage.jsx";
+import PortalSupportRecoveryPage from "./pages/PortalSupportRecoveryPage.jsx";
 import AppointmentResponsePage from "./pages/AppointmentResponsePage.jsx";
 import ChangePasswordPage from "./pages/portal/ChangePasswordPage.jsx";
 import NotificationSettingsPage from "./pages/portal/NotificationSettingsPage.jsx";
@@ -123,6 +124,7 @@ export default function App() {
         <Route path="/portal/login" element={<PortalLoginPage />} />
         <Route path="/portal/forgot-password" element={<PortalForgotPasswordPage />} />
         <Route path="/portal/reset-password" element={<PortalResetPasswordPage />} />
+        <Route path="/portal/support-recovery" element={<PortalSupportRecoveryPage />} />
         <Route path="/appointment/respond" element={<AppointmentResponsePage />} />
         <Route path="/portal/change-password" element={<ChangePasswordPage />} />
         <Route path="/portal/settings" element={<NotificationSettingsPage />} />
