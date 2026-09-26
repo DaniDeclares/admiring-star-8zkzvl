e20d529f705039e5332871078a20e978d945cf4e
DaniDeclares <danideclaresns@gmail.com>
2026-09-26 01:25:19 -0400

    Expose /api/integrations/quickbooks/start on Netlify

---
+++ netlify/functions/integrations-quickbooks-start.mjs
@@ -0,0 +1,4 @@
+import handler from "../../../api/integrations/quickbooks/start.js";
+import { runLegacyHandler } from "../_shared/legacyAdapter.mjs";
+export default (request, context) => runLegacyHandler(request, context, handler);
+export const config = { path: "/api/integrations/quickbooks/start" };
