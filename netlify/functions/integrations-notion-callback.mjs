6d87a04753da0a385ee9715922d2818da150075b
DaniDeclares <danideclaresns@gmail.com>
2026-09-26 01:25:17 -0400

    Expose /api/integrations/notion/callback on Netlify

---
+++ netlify/functions/integrations-notion-callback.mjs
@@ -0,0 +1,4 @@
+import handler from "../../../api/integrations/notion/callback.js";
+import { runLegacyHandler } from "../_shared/legacyAdapter.mjs";
+export default (request, context) => runLegacyHandler(request, context, handler);
+export const config = { path: "/api/integrations/notion/callback" };
