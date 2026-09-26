6e83d43dc16292d7d7df855d238a2aed2f96ac5b
DaniDeclares <danideclaresns@gmail.com>
2026-09-26 01:24:59 -0400

    Expose /api/integrations/hubspot/callback on Netlify

---
+++ netlify/functions/integrations-hubspot-callback.mjs
@@ -0,0 +1,4 @@
+import handler from "../../../api/integrations/hubspot/callback.js";
+import { runLegacyHandler } from "../_shared/legacyAdapter.mjs";
+export default (request, context) => runLegacyHandler(request, context, handler);
+export const config = { path: "/api/integrations/hubspot/callback" };
